4088b1fbb5a0ecd7d84b178818f01ca96c7d3d9a
Hlib Semeniuk <6918493@stud.nau.edu.ua>
2022-12-11 19:46:57 +0200

    Add Director Entity

--- DAL/Entitys/Director.cs
+++ DAL/Entitys/Director.cs
@@ -1,15 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DAL.Entitys
 {
-    public class Actor
+    public class Director
     {
-        public int Id { get; set; }
+        public int ID { get; set; }
         public string Name { get; set; }
         public DateTime Birthdate { get; set; }
         public string Country { get; set; }
