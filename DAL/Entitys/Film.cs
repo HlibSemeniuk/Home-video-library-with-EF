a21b919177fe46746ee0e610114abcf018966eb7
Hlib Semeniuk <6918493@stud.nau.edu.ua>
2022-12-11 19:50:15 +0200

    Add Film Entity

---
+++ DAL/Entitys/Film.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entitys
+{
+    public class Film
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Country { get; set; }
+        public decimal Budget { get; set; }
+    }
+}
