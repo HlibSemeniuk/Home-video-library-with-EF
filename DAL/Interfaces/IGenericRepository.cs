cbb829585239cc94d94545cfce1e0cc84b4770f2
Hlib Semeniuk <6918493@stud.nau.edu.ua>
2022-12-11 23:14:42 +0200

    Add IGernericRepository

---
+++ DAL/Interfaces/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        //TO DO:
+        //Додати Find
+
+        IEnumerable<T> GetAll();
+        T GetByID(int id);
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
