1618357624de4b77c350013b3c1450d2c13e631e
Hlib Semeniuk <6918493@stud.nau.edu.ua>
2022-12-12 20:41:44 +0200

    Add IUnitOfWork

---
+++ DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using DAL.Entitys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<Film> Films { get; }
+        IGenericRepository<Actor> Actors { get; }
+        IGenericRepository<Director> Directors { get; }
+        IGenericRepository<Genre> Genres { get; }
+        IGenericRepository<Review> Reviews { get; }
+
+        void Save();
+    }
+}
