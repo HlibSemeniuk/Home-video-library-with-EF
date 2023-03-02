8f471268c4e685cb09d081b4536e29f963d09e5a
Hlib Semeniuk <6918493@stud.nau.edu.ua>
2023-03-02 21:00:57 +0200

    Add Update method override

--- DAL/Repositories/DirectorRepository.cs
+++ DAL/Repositories/DirectorRepository.cs
@@ -13,5 +13,31 @@ namespace DAL.Repositories
         public DirectorRepository(HomeVideoLibraryContext context) : base(context)
         {
         }
+
+        public override void Update(Director director)
+        {
+            Director EF = _context.Directors.Find(director.ID);
+
+            EF.Name = director.Name;
+            EF.Description = director.Description;
+            EF.Birthdate = director.Birthdate;
+            EF.Country = director.Country;
+
+            for (int i = 0; i < EF.Films.Count; i++)
+            {
+                if (!director.Films.ToList().Exists(x => x.Name == EF.Films.ToList()[i].Name))
+                {
+                    string Name = EF.Films.ToList()[i].Name;
+                    EF.Films.Remove(_context.Films.Where(x => x.Name == Name).First());
+
+                    i--; // as Count of films change dynamically
+                }
+            }
+
+            foreach (Film f in director.Films)
+            {
+                EF.Films.Add(_context.Films.Where(x => x.Name == f.Name).First());
+            }
+        }
     }
 }
