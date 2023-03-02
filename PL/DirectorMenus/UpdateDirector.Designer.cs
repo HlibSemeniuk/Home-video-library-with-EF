247f7ee9bcca5a4b5fe82b61ac554d85a404fab2
Hlib Semeniuk <6918493@stud.nau.edu.ua>
2023-03-02 21:03:48 +0200

    Add UpdateDirector menu

---
+++ PL/DirectorMenus/UpdateDirector.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace PL.DirectorMenus
+{
+    partial class UpdateDirector
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button_Update = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.maskedTextBox_Birthdate = new System.Windows.Forms.MaskedTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox_Description = new System.Windows.Forms.TextBox();
+            this.textBox_Country = new System.Windows.Forms.TextBox();
+            this.textBox_Name = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(177, 408);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Скасувати";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button_Update
+            // 
+            this.button_Update.Location = new System.Drawing.Point(177, 386);
+            this.button_Update.Name = "button_Update";
+            this.button_Update.Size = new System.Drawing.Size(75, 23);
+            this.button_Update.TabIndex = 10;
+            this.button_Update.Text = "Змінити";
+            this.button_Update.UseVisualStyleBackColor = true;
+            this.button_Update.Click += new System.EventHandler(this.button_Update_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.checkedListBox1);
+            this.panel1.Controls.Add(this.maskedTextBox_Birthdate);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.textBox_Description);
+            this.panel1.Controls.Add(this.textBox_Country);
+            this.panel1.Controls.Add(this.textBox_Name);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(409, 368);
+            this.panel1.TabIndex = 9;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(110, 176);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(263, 124);
+            this.checkedListBox1.TabIndex = 6;
+            // 
+            // maskedTextBox_Birthdate
+            // 
+            this.maskedTextBox_Birthdate.Location = new System.Drawing.Point(110, 34);
+            this.maskedTextBox_Birthdate.Mask = "00/00/0000";
+            this.maskedTextBox_Birthdate.Name = "maskedTextBox_Birthdate";
+            this.maskedTextBox_Birthdate.Size = new System.Drawing.Size(263, 20);
+            this.maskedTextBox_Birthdate.TabIndex = 6;
+            this.maskedTextBox_Birthdate.ValidatingType = typeof(System.DateTime);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(22, 176);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Фільмографія:";
+            // 
+            // textBox_Description
+            // 
+            this.textBox_Description.Location = new System.Drawing.Point(110, 79);
+            this.textBox_Description.Multiline = true;
+            this.textBox_Description.Name = "textBox_Description";
+            this.textBox_Description.Size = new System.Drawing.Size(263, 88);
+            this.textBox_Description.TabIndex = 9;
+            // 
+            // textBox_Country
+            // 
+            this.textBox_Country.Location = new System.Drawing.Point(110, 55);
+            this.textBox_Country.Name = "textBox_Country";
+            this.textBox_Country.Size = new System.Drawing.Size(263, 20);
+            this.textBox_Country.TabIndex = 8;
+            // 
+            // textBox_Name
+            // 
+            this.textBox_Name.Location = new System.Drawing.Point(110, 12);
+            this.textBox_Name.Name = "textBox_Name";
+            this.textBox_Name.Size = new System.Drawing.Size(263, 20);
+            this.textBox_Name.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(68, 82);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Опис:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(60, 58);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Країна:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Дата народження:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(75, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ім\'я:";
+            // 
+            // UpdateDirector
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button_Update);
+            this.Controls.Add(this.panel1);
+            this.Name = "UpdateDirector";
+            this.Text = "UpdateDirector";
+            this.Load += new System.EventHandler(this.UpdateDirector_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button_Update;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox_Birthdate;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox_Description;
+        private System.Windows.Forms.TextBox textBox_Country;
+        private System.Windows.Forms.TextBox textBox_Name;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ PL/DirectorMenus/UpdateDirector.cs
@@ -0,0 +1,91 @@
+﻿using BLL.DTO;
+using BLL.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PL.DirectorMenus
+{
+    public partial class UpdateDirector : Form
+    {
+        DataGridViewRow row;
+        DirectorService directorService;
+        FilmService filmService;
+
+        public UpdateDirector(DataGridViewRow row, DirectorService directorService, FilmService filmService)
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+            this.row = row;
+            this.directorService = directorService;
+            this.filmService = filmService;
+        }
+
+        private void UpdateDirector_Load(object sender, EventArgs e)
+        {
+            FillTextBoxes();
+        }
+
+        private void FillTextBoxes()
+        {
+            textBox_Name.Text = row.Cells[1].Value.ToString();
+            maskedTextBox_Birthdate.Text = row.Cells[2].Value.ToString();
+            textBox_Country.Text = row.Cells[3].Value.ToString();
+            textBox_Description.Text = row.Cells[4].Value.ToString();
+
+            string filmsName = row.Cells[5].Value.ToString();
+
+
+            MatchCollection matchList = Regex.Matches(filmsName, @"«(.*?)»");
+            List<string> filmNames = matchList.Cast<Match>().Select(m => m.Value.Substring(1, m.Value.Length - 2)).ToList();
+
+            int i = 0;
+
+            List<FilmDTO> films = filmService.GetAll();
+
+
+            foreach (FilmDTO film in films)
+            {
+                checkedListBox1.Items.Add(film.Name);
+                i++;
+            }
+
+            foreach (string name in filmNames)
+            {
+                for (int j = 0; j < checkedListBox1.Items.Count; j++)
+                {
+                    if (name.Equals(checkedListBox1.Items[j]))
+                    {
+                        checkedListBox1.SetItemChecked(j, true);
+                    }
+                }
+            }
+        }
+
+        private void button_Update_Click(object sender, EventArgs e)
+        {
+            DirectorDTO director = new DirectorDTO() { Id = (int)row.Cells[0].Value, Name = textBox_Name.Text, Birthdate = Convert.ToDateTime(maskedTextBox_Birthdate.Text), Country = textBox_Country.Text, Description = textBox_Description.Text };
+
+            List<FilmDTO> films = new List<FilmDTO>();
+
+            foreach (string filmName in checkedListBox1.CheckedItems)
+            {
+                FilmDTO temp = new FilmDTO() { Name = filmName };
+                films.Add(temp);
+            }
+
+            director.Films = films;
+
+            directorService.ChangeData(director);
+
+            this.Close();
+        }
+    }
+}
