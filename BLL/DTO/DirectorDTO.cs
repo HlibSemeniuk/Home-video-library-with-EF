ff46ba2f40ee06cee559451c342647b84dfec905
Hlib Semeniuk <6918493@stud.nau.edu.ua>
2023-03-02 20:57:44 +0200

    Add IFilmCrew for ActorDTO and DirectorDTO

--- BLL/DTO/DirectorDTO.cs
+++ BLL/DTO/DirectorDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BLL.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,9 @@ using System.Threading.Tasks;
 
 namespace BLL.DTO
 {
-    public class DirectorDTO : INameable
+    public class DirectorDTO : INameable, IFilmCrew
     {
-        public int ID { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public DateTime Birthdate { get; set; }
         public string Country { get; set; }
