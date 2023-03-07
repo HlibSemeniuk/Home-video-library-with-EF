a0dcaf24a6b2735a4b6bde9b729f89313aa1d03b
Hlib Semeniuk <6918493@stud.nau.edu.ua>
2023-03-07 19:41:43 +0200

    Add IFilmCrew

---
+++ BLL/IFilmCrew.cs
@@ -0,0 +1,20 @@
+﻿using BLL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces
+{
+    public interface IFilmCrew
+    {
+         int Id { get; set; }
+         string Name { get; set; }
+         DateTime Birthdate { get; set; }
+         string Country { get; set; }
+         string Description { get; set; }
+
+        ICollection<FilmDTO> Films { get; set; }
+    }
+}
