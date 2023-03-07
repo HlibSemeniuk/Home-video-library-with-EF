81f0da7397fd416f1849c0758bd1c8c86530a796
Hlib Semeniuk <6918493@stud.nau.edu.ua>
2023-03-07 19:41:59 +0200

    Add ReviewService

---
+++ BLL/Services/ReviewService.cs
@@ -0,0 +1,71 @@
+﻿using AutoMapper;
+using BLL.DTO;
+using DAL;
+using DAL.Entitys;
+using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class ReviewService
+    {
+        public IUnitOfWork Database { get; set; }
+
+        public ReviewService(string connectionString)
+        {
+            Database = new UnitOfWork(connectionString);
+        }
+
+        public void Add(ReviewDTO dto)
+        {
+            Mapper.CreateMap<ActorDTO, Actor>();
+            Mapper.CreateMap<FilmDTO, Film>();
+            Mapper.CreateMap<ReviewDTO, Review>();
+            Mapper.CreateMap<DirectorDTO, Director>();
+            Mapper.CreateMap<GenreDTO, Genre>();
+
+            Review review = Mapper.Map<ReviewDTO, Review>(dto);
+            
+            Database.Reviews.Insert(review);
+            Database.Save();
+        }
+
+        public List<ReviewDTO> GetAll(int filmID)
+        {
+            Mapper.CreateMap<Actor, ActorDTO>();
+            Mapper.CreateMap<Film, FilmDTO>();
+            Mapper.CreateMap<Review, ReviewDTO>();
+            Mapper.CreateMap<Director, DirectorDTO>();
+            Mapper.CreateMap<Genre, GenreDTO>();
+
+            Func<Review, bool> filmIDMatch = r => r.Film.ID == filmID;
+
+            return Mapper.Map<IEnumerable<Review>, List<ReviewDTO>>(Database.Reviews.Find(filmIDMatch));
+        }
+
+        public void Delete(int id)
+        {
+            Database.Reviews.Delete(id);
+            Database.Save();
+        }
+
+        public void ChangeData(ReviewDTO dto)
+        {
+            Mapper.CreateMap<ActorDTO, Actor>();
+            Mapper.CreateMap<FilmDTO, Film>();
+            Mapper.CreateMap<ReviewDTO, Review>();
+            Mapper.CreateMap<DirectorDTO, Director>();
+            Mapper.CreateMap<GenreDTO, Genre>();
+
+            Review review = Mapper.Map<ReviewDTO, Review>(dto);
+
+            Database.Reviews.Update(review);
+            Database.Save();
+        }
+    }
+}
